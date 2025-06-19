e4ea38707a6285d5690389b47176c3d8d6400e6a
Vladyslav Rozhenko <pochta.vlad@ukr.net>
2025-06-19 13:00:26 +0300

    fix build

--- backend/dist/db.js
+++ backend/dist/db.js
@@ -9,10 +9,12 @@ const { PGHOST, PGPORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = proces
 if (!PGHOST || !POSTGRES_USER || !POSTGRES_PASSWORD || !POSTGRES_DB) {
     throw new Error('❌ Required environment variables for database are missing');
 }
-export const pool = new Pool({
-    host: PGHOST,
-    port: Number(PGPORT) || 5432,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    database: POSTGRES_DB,
-});
+const config = {
+    host: process.env.PGHOST,
+    port: Number(process.env.PGPORT),
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    family: 4,
+};
+export const pool = new Pool(config);
